ea209eca3f7c02c78dccd1dca887c816d2de4200
marcuses101 <mnjconnolly@gmail.com>
2020-10-01 00:43:28 -0400

    Accordion with hooks

--- src/state-drills/Accordion.js
+++ src/state-drills/Accordion.js
@@ -1,43 +1,75 @@
-import React, { Component } from "react";
-import "./Accordion.css"
+import React, { Component, useState } from "react";
+import "./Accordion.css";
 
-export default class Accordion extends Component {
-  static defaultProps = {};
-  constructor(props) {
-    super(props);
+// export default class Accordion extends Component {
+//   static defaultProps = {};
+//   constructor(props) {
+//     super(props);
 
-    this.state = {
-      currentSectionIndex: null,
-    };
-  }
-  renderContent(index,content){
-    if (index === this.state.currentSectionIndex) {
-      return <p>{content}</p>
-    }
-  }
+//     this.state = {
+//       currentSectionIndex: null,
+//     };
+//   }
+//   renderContent(index,content){
+//     if (index === this.state.currentSectionIndex) {
+//       return <p>{content}</p>
+//     }
+//   }
 
-  handleClick = (index) => {
-    const newState = this.state.currentSectionIndex !== index? index: null;
-    this.setState({ currentSectionIndex: newState });
+//   handleClick = (index) => {
+//     const newState = this.state.currentSectionIndex !== index? index: null;
+//     this.setState({ currentSectionIndex: newState });
+//   };
+
+//   render() {
+//     const sections = this.props.sections?.map(({ title, content }, index) => {
+//       return (
+//         <li key={index}>
+//           <button
+//             onClick={() => {
+//               this.handleClick(index);
+//             }}
+//           >
+//             {title}
+//           </button>
+//           {this.renderContent(index,content)}
+//         </li>
+//       );
+//     });
+
+//     return <ul className="Accordion">{sections || <li/>}</ul>;
+//   }
+// }
+
+export default function Accordion(props) {
+  const { sections } = props;
+  const [currentSectionIndex, setCurrentSectionIndex] = useState(null);
+
+  const handleClick = (index) => {
+    const newState = currentSectionIndex !== index ? index : null;
+    setCurrentSectionIndex(newState);
   };
 
+  const renderContent = (index, content) => {
+    if (index === currentSectionIndex) {
+      return <p>{content}</p>;
+    }
+  };
 
-  render() {
-    const sections = this.props.sections?.map(({ title, content }, index) => {
-      return (
-        <li key={index}>
-          <button
-            onClick={() => {
-              this.handleClick(index);
-            }}
-          >
-            {title}
-          </button>
-          {this.renderContent(index,content)}
-        </li>
-      );
-    });
+  const listItems = sections?.map(({ title, content }, index) => {
+    return (
+      <li key={index}>
+        <button
+          onClick={() => {
+            handleClick(index);
+          }}
+        >
+          {title}
+        </button>
+        {renderContent(index, content)}
+      </li>
+    );
+  });
 
-    return <ul className="Accordion">{sections || <li/>}</ul>;
-  }
+  return <ul className="Accordion">{listItems || <li />}</ul>;
 }
